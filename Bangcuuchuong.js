2dd306fcad1de5c3dc5c047224d353a7bf3f52ed
than <user@users-MacBook-Pro.local>
2023-03-15 10:53:15 +0700

    BT_Vonglap

---
+++ Bangcuuchuong.js
@@ -0,0 +1,8 @@
+<script>
+    for (var i=2; i<=10; i++){
+        for(var j =2; j<=10; j++){
+            var product = i*j;
+    document.write(i + " x " + j + " = " + product + "<br>");
+}
+}
+</script>
